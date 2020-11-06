4eb355f819e744933c9d247d6d14afa99837f31d
Ann <heb2021@gmail.com>
2020-11-06 14:24:27 +0900

    3.0v

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
 import React, { Component } from "react";
 import './App.css';
-import data from "./data";
-import ViewImage from "./components/viewImage";
-//import axios from "axios";
-
-class Header extends Component {
-  render(){
-    return (
-      <div className="header">
-        <h2>사진갤러리</h2>
-      </div>
-    );
-  }
-}
+import ImageData from "./imageData";
 
 class App extends Component {
   constructor(props){
@@ -21,10 +9,10 @@ class App extends Component {
     this.max_id = 2;
 
     this.state = {
-      properties: data.properties,
-      property1: data.properties[0],
-      property2: data.properties[1],
-      property3: data.properties[2]
+      images: ImageData.images,
+      firstImage: ImageData.images[0],
+      secondImage: ImageData.images[1],
+      thirdImage: ImageData.images[2],
     }
     
     this.handleChange = this.handleChange.bind(this)
@@ -32,81 +20,61 @@ class App extends Component {
 
 /* Pagination */
 prePagination = () => {
-  const newIndex = this.max_id
+  const endId = this.max_id
+  const startId = 0;
+  const copyPrevArray = [];
 
-  if (this.state.property1.index === 0) {
-    this.setState({
-      property1: this.state.properties[newIndex],
-      property2: this.state.properties[this.state.property2.index-1],
-      property3: this.state.properties[this.state.property3.index-1]
-    })
-  } else if (this.state.property2.index === 0) {
-    this.setState({
-      property1: this.state.properties[this.state.property1.index-1],
-      property2: this.state.properties[newIndex],
-      property3: this.state.properties[this.state.property3.index-1]})
-  } else if (this.state.property3.index === 0) {
-    this.setState({
-      property1: this.state.properties[this.state.property1.index-1],
-      property2: this.state.properties[this.state.property2.index-1],
-      property3: this.state.properties[newIndex]})
-  } else {
-    this.setState({
-      property1: this.state.properties[this.state.property1.index-1],
-      property2: this.state.properties[this.state.property2.index-1],
-      property3: this.state.properties[this.state.property3.index-1]
-    })
+  copyPrevArray[endId] = this.state.images[startId];
+
+  for (var i=0; i<endId; i++) {
+    copyPrevArray[i] = this.state.images[i+1];
   }
+  
+  this.setState({
+    images: copyPrevArray,
+    firstImage: copyPrevArray[0],
+    secondImage: copyPrevArray[1],
+    thirdImage: copyPrevArray[2]
+  })
 }
 
-  nextPagination = () => {
-    const newIndex = 0
-    if (this.state.property1.index === this.max_id) {
-      this.setState({
-        property1: this.state.properties[newIndex],
-        property2: this.state.properties[this.state.property2.index+1],
-        property3: this.state.properties[this.state.property3.index+1]
-      })
-    } else if (this.state.property2.index === this.max_id) {
-      this.setState({
-        property1: this.state.properties[this.state.property1.index+1],
-        property2: this.state.properties[newIndex],
-        property3: this.state.properties[this.state.property3.index+1]
-      })
-    } else if (this.state.property3.index === this.max_id) {
-      this.setState({
-        property1: this.state.properties[this.state.property1.index+1],
-        property2: this.state.properties[this.state.property2.index+1],
-        property3: this.state.properties[newIndex]
-      })
-    } else {
-      this.setState({
-        property1: this.state.properties[this.state.property1.index+1],
-        property2: this.state.properties[this.state.property2.index+1],
-        property3: this.state.properties[this.state.property3.index+1]
-      })
+nextPagination = () => {
+    const endId = this.max_id
+    const copyNextArray = [];
+  
+    copyNextArray[0] = this.state.images[endId];
+  
+    for (var i=1; i<=endId; i++) {
+      copyNextArray[i] = this.state.images[i-1];
     }
+  
+    this.setState({
+      images: copyNextArray,
+      firstImage: copyNextArray[0],
+      secondImage: copyNextArray[1],
+      thirdImage: copyNextArray[2]
+    })
   }
-
 
   handleChange(event) {
     this.max_id=this.max_id+1;
 
-    var _properties = this.state.properties.concat(
-      {index:this.max_id, picture:URL.createObjectURL(event.target.files[0])}
+    var _images = this.state.images.concat(
+      {index:this.max_id, picture:URL.createObjectURL(event.target.files[0]), name:event.target.files[0].name}
     )
-
     this.setState({
-      properties:_properties
+      images:_images
     });
   }
 
   render() {
-    const {property1, property2, property3} = this.state;
+    const {firstImage, secondImage, thirdImage} = this.state;
 
     return (
       <div>
-        <Header></Header>
+        <div className="header">
+          <h2>사진갤러리</h2>
+        </div>
 
         <div className="pagination">
           <button className="previous"
@@ -121,9 +89,15 @@ prePagination = () => {
         </div>
 
         <div className="viewImage">
-          <img src={property1.picture} alt="picture_1" className="img1"/>
-          <img src={property2.picture} alt="picture_2" className="img2"/>
-          <img src={property3.picture} alt="picture_3" className="img3"/>
+          <img src={firstImage.picture} alt="picture_1" className="img1"/>
+          <img src={secondImage.picture} alt="picture_2" className="img2"/>
+          <img src={thirdImage.picture} alt="picture_3" className="img3"/>
+        </div>
+
+        <div className="fileName">
+          <div className="name1">{firstImage.name}</div>
+          <div className="name2">{secondImage.name}</div>
+          <div className="name3">{thirdImage.name}</div>
         </div>
 
         <div className="FileInput">
@@ -134,6 +108,7 @@ prePagination = () => {
           <input
           type="file"
           id="ex_file"
+          name="excelFile"
           accept="image/jpeg, image/png"
           onChange={this.handleChange}
           ref={fileInput => this.fileInput = fileInput}
--- src/imageData.js
+++ src/imageData.js
@@ -3,18 +3,21 @@ import image_2 from './images/image_2.jpg';
 import image_3 from './images/image_3.jpg';
 
 const data = {
-    "properties": [
+    "images": [
     {
         "index": 0,
-        "picture": image_1
+        "picture": image_1,
+        "name": "image_1"
     },
     {
         "index": 1,
-        "picture": image_2
+        "picture": image_2,
+        "name": "image_2"
     },
     {
         "index": 2,
-        "picture": image_3
+        "picture": image_3,
+        "name": "image_3"
     }
     ]
 }
